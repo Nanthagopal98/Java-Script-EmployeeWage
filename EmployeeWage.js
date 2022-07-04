547501afd1cf617a140a2f5c92405b4076fb04f3
unknown <s.nanthagopal1998@gmail.com>
2022-07-04 15:09:09 +0530

    [Nantha] - UC-1-Added

---
+++ EmployeeWage.js
@@ -0,0 +1,14 @@
+const EMP_ABSENT = 0;
+let check = Math.floor(Math.random() * 2);  // Math.floor(Math.random() * 10) % 2
+console.log(check);
+if(check == EMP_ABSENT)
+{
+    console.log("Employee Absent");
+}
+else
+{
+    console.log("Employee Present");
+}
+
+
+
