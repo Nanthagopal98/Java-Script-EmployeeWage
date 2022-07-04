be672fe65cbcfd76495dde281db4ba13233a1a85
unknown <s.nanthagopal1998@gmail.com>
2022-07-04 15:32:25 +0530

    [Nantha] - Maximum Working Hours Limited

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,10 +1,12 @@
 const EMP_FULL_TIME = 1;
 const EMP_PART_TIME = 2;
 const WAGE_PER_HOUR = 20;
-const TOTAL_WORKING_DAYS = 20;
+const MAX_WORKING_DAYS = 20;
+const MAX_WORKING_HOURS = 160;
 const FULL_TIME = 8;
 const PART_TIME = 4;
 let totalWorkingHour = 0;
+let day = 1;
 
 function getWorkingHour(check)
 {
@@ -18,10 +20,11 @@ function getWorkingHour(check)
         return workingHour = 0;
     }
 }
-for (let day = 1; day < TOTAL_WORKING_DAYS; day++)
+while( day < MAX_WORKING_DAYS && totalWorkingHour < MAX_WORKING_HOURS)
 {
     let check = Math.floor(Math.random() * 3);
     totalWorkingHour += getWorkingHour(check);
+    day++;
 }
 let totalSalary = totalWorkingHour * WAGE_PER_HOUR;
-console.log("Today Salary : "+totalSalary);
+console.log("Total Working Hours : "+totalWorkingHour + " Total Working Days : "+ day + " Today Salary : "+totalSalary);
