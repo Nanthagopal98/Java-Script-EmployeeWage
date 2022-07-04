670b2ee20ba03705014dd25a860f6b9b655938a8
unknown <s.nanthagopal1998@gmail.com>
2022-07-04 15:10:48 +0530

    [Nantha] - Employee Absent or Present Checcked

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,5 +1,5 @@
 const EMP_ABSENT = 0;
-let check = Math.floor(Math.random() * 2);  // Math.floor(Math.random() * 10) % 2
+let check = Math.floor(Math.random() * 2);   // Math.floor(Math.random() * 10) % 2
 console.log(check);
 if(check == EMP_ABSENT)
 {
