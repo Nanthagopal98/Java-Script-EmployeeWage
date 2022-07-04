5919f09b588c76533cefc38070a09a212463cfa4
unknown <s.nanthagopal1998@gmail.com>
2022-07-04 15:17:53 +0530

    [Nantha] - Part Time Added And Salary Calculated

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,14 +1,26 @@
-const EMP_ABSENT = 0;
-let check = Math.floor(Math.random() * 2);   // Math.floor(Math.random() * 10) % 2
-console.log(check);
-if(check == EMP_ABSENT)
+const EMP_FULL_TIME = 1;
+const EMP_PART_TIME = 2;
+const WAGE_PER_HOUR = 20;
+const FULL_TIME = 8;
+const PART_TIME = 4;
+let workingHour;
+let check = Math.floor(Math.random() * 3);
+switch(check)
 {
-    console.log("Employee Absent");
+    case 1:
+        workingHour = FULL_TIME;
+        console.log("Employee Working Full Time");
+        break;
+    case 2:
+        workingHour = PART_TIME;
+        console.log("Employee Working Part Time");
+        break;
+    default:
+        workingHour = 0;
+        console.log("Employee Absent");
+        break;
 }
-else
-{
-    console.log("Employee Present");
-}
-
+employeeSalary = workingHour * WAGE_PER_HOUR;
+console.log("Today Salary : "+employeeSalary);
 
 
