33184b69fa74c32a227603d23058b2b0a3198e8e
unknown <s.nanthagopal1998@gmail.com>
2022-07-06 16:32:10 +0530

    [Nantha] - Employee Payroll Class Extended To Add Gender And Start Date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -3,10 +3,12 @@ class EmployeePayrollData {
     id;
     salary;
 
-    constructor(id, name, salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
+    constructor(...details) {
+        this.id = details[0];
+        this.name = details[1];
+        this.salary = details[2];
+        this.gender = details[3];
+        this.startDate = details[4];
     }
 
     get name() {
@@ -17,7 +19,11 @@ class EmployeePayrollData {
         this._name = name;
     }
     toString() {
-        return "Id: " + this.id + " Name: " + this.name + " Salary: " + this.salary;
+        const option = { year: "numeric", month: "long", day: "numeric" };
+        const joiningDate = this.startDate === undefined ? "undefined" :
+            this.startDate.toLocaleDateString("en-US", option);
+        return "Id: " + this.id + " Name: " + this.name + " Salary: " + this.salary +
+            " Gender: " + this.gender + " Start Date: " + joiningDate;
     }
 }
 let employeePayrollData = new EmployeePayrollData(1, "Nantha", 50000);
@@ -25,3 +31,5 @@ console.log(employeePayrollData.toString());
 employeePayrollData.name = "Mani";
 employeePayrollData.salary = 45000;
 console.log(employeePayrollData.toString());
+let AddedEmpDetail = new EmployeePayrollData(2,"Madhu",25000,"F",new Date());
+console.log(AddedEmpDetail.toString());
